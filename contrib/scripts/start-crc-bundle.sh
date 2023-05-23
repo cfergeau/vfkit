73364d6e7f4042517fb9a11d7df5bae93b8da636
Christophe Fergeau <cfergeau@redhat.com>
2023-05-23 13:58:32 +0200

    hack: Change start-bundle.sh for testing

--- contrib/scripts/start-crc-bundle.sh
+++ contrib/scripts/start-crc-bundle.sh
@@ -8,13 +8,13 @@ KERNEL=$(cat ${BUNDLE_PATH}/crc-bundle-info.json | ${YQ} .nodes[0].kernel)
 INITRD=$(cat ${BUNDLE_PATH}/crc-bundle-info.json | ${YQ} .nodes[0].initramfs)
 CMDLINE=$(cat ${BUNDLE_PATH}/crc-bundle-info.json | ${YQ} .nodes[0].kernelCmdLine)
 DISKIMG=$(cat ${BUNDLE_PATH}/crc-bundle-info.json | ${YQ} .storage.diskImages[0].name)
-cp -c ${BUNDLE_PATH}/${DISKIMG} overlay.img
+#cp -c ${BUNDLE_PATH}/${DISKIMG} overlay.img
 
 ./out/vfkit --cpus 2 --memory 2048 \
     --kernel "${BUNDLE_PATH}/${KERNEL}" \
     --initrd "${BUNDLE_PATH}/${INITRD}" \
     --kernel-cmdline "${CMDLINE}" \
     --device virtio-blk,path=overlay.img \
-    --device virtio-serial,logFilePath=start-bundle.log \
-    --device virtio-net,nat,mac=72:20:43:d4:38:62 \
+    --device virtio-serial,stdio \
+    --device virtio-net,unixSocketPath=gvproxy.sock,mac=5a:94:ef:e4:0c:ee \
     --device virtio-rng
